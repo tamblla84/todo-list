6fc7df68cf62ecbbc26ec1d34ab850c3227188ca
Tam huu <tamblla1984@gmail.com>
2025-04-09 22:41:41 +0700

    Them tinh nang nhap viec can lam

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("To-Do List khởi động!");
+
+function addTodo() {
+    const input = document.getElementById("todo-input").value;
+    const list = document.getElementById("todo-list");
+    const item = document.createElement("li");
+    item.textContent = input;
+    list.appendChild(item);
+    document.getElementById("todo-input").value = "";
+}
